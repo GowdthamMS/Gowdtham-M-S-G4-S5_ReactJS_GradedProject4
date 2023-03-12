47edf3386f1dddef966ca6f6d04bbeb6e9a250fa
GowdthamMS <105445738+GowdthamMS@users.noreply.github.com>
2023-03-13 03:06:38 +0530

    updated

--- movies-client/src/components/Model.tsx
+++ movies-client/src/components/Model.tsx
@@ -1,6 +1,6 @@
 import IMovieDetail from "../models/IMovieDetail";
 
-const Model = ({title, posterurl, storyline, closeModel}:IMovieDetail) => {
+const Model = ({title, posterurl, storyline}:IMovieDetail) => {
     return (<>
     
     <div className="model-window">
@@ -9,6 +9,7 @@ const Model = ({title, posterurl, storyline, closeModel}:IMovieDetail) => {
       <img className="img" src={posterurl} alt="movie item" />
       <br/>
       <h2>Story Line</h2>
+      <button className="ellipsis" onClick={() => {}}></button>
       <p className="ellipsis">{storyline}</p>
     </div>
   </div>
